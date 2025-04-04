1fbfb2e231a815b65ba05b5081de74eb3c7f7905
Emmanuel Simisi <esimisi@alvariumgroup.com>
2025-04-04 00:27:10 +0000

    fixe update user

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -26,6 +26,11 @@ class EditUser extends EditRecord
         $data['slug'] = $slug;
         $roleName = $data['role_user'] ?? '';
 
+        if(!empty($data['changePassword'])){
+            unset($data['changePassword']);
+            unset($data['password_confirmation']);
+        }
+
         //unset($data['role']);
 
         if (!empty($roleName)) {
