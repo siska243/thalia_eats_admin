79067fe630d6dc8a149c7f9907739486ca19fd68
Emmanuel Simisi <esimisi@alvariumgroup.com>
2025-04-14 23:59:53 +0100

    gestion fcm token

--- app/Wrappers/FirebasePushNotification.php
+++ app/Wrappers/FirebasePushNotification.php
@@ -1,32 +1,44 @@
 <?php
 
 namespace App\Wrappers;
+
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
+
 class FirebasePushNotification
 {
-    public function sendPushNotification($deviceToken, $title, $body,string |null $image=null)
+    public function sendPushNotification($deviceToken, $title, $body, string|null $image = null)
     {
         $messaging = app('firebase.messaging');
 
         $message = CloudMessage::new()
-            ->withNotification(Notification::create($title, $body,$image))
+            ->withNotification(Notification::create($title, $body, $image))
             ->withDefaultSounds()
             ->toToken($deviceToken);
 
-        $messaging->send($message);
+        try {
+            $messaging->send($message);
+        } catch (\Exception $exception) {
+
+        }
+
     }
 
-    public function sendPushNotificationMultiUser(array $deviceTokens, $title, $body,string |null $image=null)
+    public function sendPushNotificationMultiUser(array $deviceTokens, $title, $body, string|null $image = null)
     {
         $messaging = app('firebase.messaging');
 
         $message = CloudMessage::new()
-            ->withNotification(Notification::create($title, $body,$image))
-            ->withDefaultSounds()
-           ;
+            ->withNotification(Notification::create($title, $body, $image))
+            ->withDefaultSounds();
 
-        $messaging->sendMulticast($message, $deviceTokens);
+        $results = $messaging->validateRegistrationTokens($deviceTokens);
+
+        $validTokens = $results->valid();
+        if (!empty($validTokens)) {
+            $messaging->sendMulticast($message, $validTokens);
+        }
+
 
     }
 
