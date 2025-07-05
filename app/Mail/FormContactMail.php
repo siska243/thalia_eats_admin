685a4b261fe5eacf480cbe1c9441e361c8d9a02e
Emmanuel Simisi <emmanuelsimisi@gmail.com>
2025-07-05 20:59:44 +0100

    add dotp et verify account

---
+++ app/Mail/FormContactMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+
+class FormContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(private $data)
+    {
+        //
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Confirmation de réception de votre message',
+            cc: [
+                new Address('contact@sims-opportunity.com', "Connect RDC")
+            ]
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact',
+            with: [
+                'email' => $this->data['email'],
+                'name' => $this->data['name'],
+                "subject" => $this->data['subject'],
+                "content" => $this->data['message'],
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
